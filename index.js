e2f0308c32c94cc8c4d79104ac603b59eeac09d1
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-30 09:37:59 -0500

    Summary
    Summary

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var scanf_1 = require("scanf");
+// Lectura simple
+console.log('Por favor ingresa tu nombre:');
+var nombre = (0, scanf_1.default)('%s');
+console.log('Por favor ingresa tu edad:');
+var edad = (0, scanf_1.default)('%d');
+console.log("Nombre: ".concat(nombre, ", Edad: ").concat(edad));
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hola")
+import scanf from 'scanf';
+
+// Lectura simple
+console.log('Por favor ingresa tu nombre:');
+const nombre = scanf('%s');
+
+console.log('Por favor ingresa tu edad:');
+const edad = scanf('%d');
+
+console.log(`Nombre: ${nombre}, Edad: ${edad}`);
+
+// RESOLVER EL PROBLEMA CON SCAF
