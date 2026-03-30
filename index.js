dd21e09e588ca824a5e6e234c4c35f0b41a1ecfd
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-30 16:12:25 -0500

    Summary
    Summary

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var scanf_1 = require("scanf");
-// Lectura simple
-console.log('Por favor ingresa tu nombre:');
-var nombre = (0, scanf_1.default)('%s');
-console.log('Por favor ingresa tu edad:');
-var edad = (0, scanf_1.default)('%d');
-console.log("Nombre: ".concat(nombre, ", Edad: ").concat(edad));
+console.log("Introduce un número y un texto (ej. 10 hola):");
+// Lee un entero (%d) y una cadena (%s)
+var result = (0, scanf_1.sscanf)('%d', '%s');
+console.log(result); // Devuelve un objeto: { '0': 10, '1': 'hola' }
--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import scanf from 'scanf';
-
-// Lectura simple
-console.log('Por favor ingresa tu nombre:');
-const nombre = scanf('%s');
-
-console.log('Por favor ingresa tu edad:');
-const edad = scanf('%d');
-
-console.log(`Nombre: ${nombre}, Edad: ${edad}`);
-
-// RESOLVER EL PROBLEMA CON SCAF
+console.log("Hola")
