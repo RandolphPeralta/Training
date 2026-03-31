1d8d11172313906562365545d4ac40b2d14dd056
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-30 20:04:43 -0500

    Summary
    Summary

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var scanf_1 = require("scanf");
-console.log("Introduce un número y un texto (ej. 10 hola):");
-// Lee un entero (%d) y una cadena (%s)
-var result = (0, scanf_1.sscanf)('%d', '%s');
-console.log(result); // Devuelve un objeto: { '0': 10, '1': 'hola' }
+// Ejemplo: Pedir un nombre (string) y una edad (number)
+console.log("Ingresa tu nombre:");
+var nombre = (0, scanf_1.default)('%s');
+console.log("Ingresa tu edad:");
+var edad = (0, scanf_1.default)('%d');
+console.log("Hola ".concat(nombre, ", el pr\u00F3ximo a\u00F1o tendr\u00E1s ").concat(Number(edad) + 1, " a\u00F1os."));
--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hola")
+import scanf from 'scanf';
+
+// Ejemplo: Pedir un nombre (string) y una edad (number)
+console.log("Ingresa tu nombre:");
+const nombre = scanf('%s');
+
+console.log("Ingresa tu edad:");
+const edad = scanf('%d');
+
+console.log(`Hola ${nombre}, el próximo año tendrás ${Number(edad) + 1} años.`);
