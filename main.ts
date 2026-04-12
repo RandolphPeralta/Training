077bc748cb417c50d084e845364edebb1755d8a5
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-04-12 11:33:30 -0500

    Update main.ts
    Update main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@ import {Application} from "./src/runapp/app"
 import {View} from "./src/ui/view";
 import {InMemoryService} from "./src/persistence/memory";
 import {Estudiante, Libro, Prestamo} from "./src/modelsave/type";
-import {BookService} from "./src/services/book.service";
-import {StudentService } from "./src/services/student.service";
-import { LoanService } from "./src/services/loans.service";
 
 const studentmemory = new InMemoryService<Estudiante>()
 const bookmemory = new InMemoryService<Libro>()
